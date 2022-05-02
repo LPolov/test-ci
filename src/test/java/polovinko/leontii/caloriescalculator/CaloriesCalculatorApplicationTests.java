e378c2d94680b10336b3a1bb9644ce1272403205
Leontii Polovinko <leontii_polovinko@epam.com>
2022-05-02 11:31:35 +0300

    initial commit

---
+++ src/test/java/polovinko/leontii/caloriescalculator/CaloriesCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package polovinko.leontii.caloriescalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
